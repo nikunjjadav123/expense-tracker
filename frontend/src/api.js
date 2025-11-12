fabfdc34997b3a0ab5c78b5be9ae6a0e160910d8
nikunjjadav123 <dirensj@gmail.com>
2025-11-12 18:14:07 +0530

    restoredapi.js file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://expense-tracker-o1dq.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getExpenses = async () => {
   const res = await axios.get(`${API_URL}/expenses/`);
