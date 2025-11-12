085ed2efdfee0df8ea8aca3398fb57d81ad17815
nikunjjadav123 <dirensj@gmail.com>
2025-11-12 18:07:48 +0530

    added backend URL hard-coded to check in api.js file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://expense-tracker-o1dq.onrender.com";
 
 export const getExpenses = async () => {
   const res = await axios.get(`${API_URL}/expenses/`);
