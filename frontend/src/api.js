500d7dd07bd4d963b64f85012174b7b124f816cd
nikunjjadav123 <dirensj@gmail.com>
2025-11-12 18:29:32 +0530

    removed backslash after endpoint in api.js file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getExpenses = async () => {
-  const res = await axios.get(`${API_URL}/expenses/`);
+  const res = await axios.get(`${API_URL}/expenses`);
   return res.data;
 };
 
 export const addExpense = async (expense) => {
-  const res = await axios.post(`${API_URL}/expenses/`, expense);
+  const res = await axios.post(`${API_URL}/expenses`, expense);
   return res.data;
 };
